98b92d8eabbc2244c427cde25bdaa827b2f02c30
xdhcvr <xdhcvr@gmail.com>
2019-03-12 05:55:14 +0700

    Добавлены аспекты AccessAspect и LoggerAspect

--- src/main/java/cvr/otus/aspects/AccessAspect.java
+++ src/main/java/cvr/otus/aspects/AccessAspect.java
@@ -5,46 +5,34 @@ import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 @Aspect
 @Component
+@Order(0)
 public class AccessAspect {
-    private final static Logger LOGGER = Logger.getLogger(AccessAspect.class);
-
-    public AccessAspect() {
-        log(" ================== ");
-        log("|Start AccessAspect|");
-        log(" ================== ");
-    }
 
     @Around(value = "execution(* cvr.otus.service.ExamRunner.run(.. )) && args(student)")
     public Exception around(ProceedingJoinPoint joinPoint, Student student) {
         String params = Arrays.toString(joinPoint.getArgs());
         String name = joinPoint.getSignature().getName();
-        log("running : " + name);
-        log("params  : " + params);
-        Object proceed = null;
+        Object proceed;
         String role = student.getRole();
         if (!"student".equals( role )){
             proceed = "Доступ пользователю с ролью[" + role+"] запрещен";
 
             System.err.println(proceed);
-            log("retVal  : " +proceed);
             return new Exception(proceed.toString());
         }
         try {
             proceed = joinPoint.proceed(joinPoint.getArgs());
-        } catch (Throwable throwable) {
-            log(throwable.getMessage());
+        } catch (Throwable err) {
+            err.printStackTrace();
         }
-        log("retVal  : " +proceed);
         return null;
     }
 
-    private void log(String logMessage) {
-        LOGGER.info(logMessage);
-    }
 }
--- src/main/java/cvr/otus/aspects/LoggerAspect.java
+++ src/main/java/cvr/otus/aspects/LoggerAspect.java
@@ -1,10 +1,12 @@
 package cvr.otus.aspects;
 
+import cvr.otus.domain.Student;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.apache.log4j.Logger;
 
@@ -12,6 +14,7 @@ import java.util.Arrays;
 
 @Aspect
 @Component
+@Order(10)
 public class LoggerAspect {
 //    private final PrintService printService = new PrintServiceImpl();
 
@@ -30,6 +33,29 @@ public class LoggerAspect {
         log("params  : " + params);
         log("retVal  : " + retVal);
         log("-----------------------------------");
+    }
+    @Around(value = "execution(* cvr.otus.service.ExamRunner.run(.. )) && args(student)")
+    public Exception around(ProceedingJoinPoint joinPoint, Student student) {
+        String params = Arrays.toString(joinPoint.getArgs());
+        String name = joinPoint.getSignature().getName();
+        log("running : " + name);
+        log("params  : " + params);
+        Object proceed = null;
+        String role = student.getRole();
+        if (!"student".equals( role )){
+            proceed = "Доступ пользователю с ролью[" + role+"] запрещен";
+
+            System.err.println(proceed);
+            log("retVal  : " +proceed);
+            return new Exception(proceed.toString());
+        }
+        try {
+            proceed = joinPoint.proceed(joinPoint.getArgs());
+        } catch (Throwable throwable) {
+            log(throwable.getMessage());
+        }
+        log("retVal  : " +proceed);
+        return null;
     }
 
     @Around(value = "execution(Double cvr.otus.service.QuestionService.checkQuestion(int,String))")
